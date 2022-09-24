b89842699ebaa55f3a59a3d6aae018371d9d3aae
Svetlana-18 <vasilevichsvetlana@yandex.ru>
2022-09-25 00:45:43 +0300

    проверила решения, подготовила работу к отправке

--- Task_2.py
+++ Task_2.py
@@ -1,13 +1,11 @@
-#2. Напишите программу для. проверки истинности утверждения
- # ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+# 2. Напишите программу для. проверки истинности утверждения
+# ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
-print ()
-print ('утверждение  ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z истинно при')
-print ('x y z')
+print()
+print('утверждение  ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z истинно при')
+print('x y z')
 for x in range(2):
     for y in range(2):
         for z in range(2):
-            if not(x or y or z) == (not x and not y and not z):
-                print (x, y, z)
-    
-            
+            if not (x or y or z) == (not x and not y and not z):
+                print(x, y, z)
